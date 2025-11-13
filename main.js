7b949e72e57ef713ea1ba119839a550c8d158461
francesco.miliotti <francesco.miliotti@email.com>
2025-11-13 17:11:19 +0100

    ultime modifiche

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 
 
-// document.getElementById("testoUtente").addEventListener("input", function() {
-// 	let x = document.getElementById("testoUtente").value.trim();
-// 	document.getElementById("result").innerText = x;
-// })
-
 
 const prezzoKm = 0.21;
 let costoBiglietto = null;
@@ -53,32 +48,3 @@ document.getElementById("utente").innerText = `Passeggero : ${name}  ${surname}`
 
 
 
-//let inputKm = null;
-//let inputEtaPasseggero = null;
-
-
-
-
-
-/*const Km = parseInt(inputKm);
-const etaPasseggero = parseInt(inputEtaPasseggero);
-
-if (etaPasseggero < 18) {
-    costoBiglietto = ((Km * prezzoKm) * (100 - 20) / 100);
-    
-
-} else if (etaPasseggero > 65) {
-    costoBiglietto = ((Km * prezzoKm) * (100 - 40) / 100);
-
-} else {
-    costoBiglietto = Km * prezzoKm;
-}
-
-
-console.log("km", inputKm, "eta", inputEtaPasseggero);
-console.log("Costo Biglietto", costoBiglietto.toFixed(2));
-
-
-/*document.getElementById("km").innerHTML = Km;
-document.getElementById("eta").innerHTML = etaPasseggero;
-document.getElementById("prezzoFinale").innerHTML = costoBiglietto.toFixed(2);*/
